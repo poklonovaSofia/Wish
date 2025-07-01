51d6802c3c81f3835e2e681811bcd6793d69deee
Sofia <sofapokl@gmail.com>
2025-07-01 15:50:02 +0300

    do PWA

---
+++ src/components/Envelope.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Envelope.module.css' ;
+export const Envelope = () => {
+    return(
+    <>
+    <div className={styles.container}>
+        <div className={styles.topOfContainer}></div>
+    </div>
+    </>);
+};
