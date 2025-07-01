eea13378eaa0feeab3f60cf9368ec344646065a6
Sofia <sofapokl@gmail.com>
2025-07-01 16:21:43 +0300

    Envelope update

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -1,9 +1,14 @@
+import { FaHeart } from 'react-icons/fa';
 import styles from '../styles/Envelope.module.css' ;
 export const Envelope = () => {
     return(
     <>
     <div className={styles.container}>
-        <div className={styles.topOfContainer}></div>
+        <div className={styles.borderOfTop}>
+           
+        </div>
+         <div className={styles.topOfContainer}></div>
+         <FaHeart className={styles.heartIcon} />
     </div>
     </>);
 };
